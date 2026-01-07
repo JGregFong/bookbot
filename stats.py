58b38052b7397c6b186d970989084100e938e007
JGregFong <jgregfong@gmail.com>
2026-01-06 17:40:37 -0800

    updated stats.py

--- stats.py
+++ stats.py
@@ -4,3 +4,29 @@ def get_num_words(text):
         number+=1
 
     return number
+
+def get_num_letters(text):
+    letters = {
+        
+    }
+    for word in text.split():
+        string = word.lower()
+        for char in string:
+            letters.setdefault(char,0)
+            letters[char] +=1  
+    return letters
+def sort_on(items):
+    return items["num"]
+def sorted_dictionary(original_list):
+    dictionary_list = []
+        
+    for item in original_list:
+        info = {
+            "char" : item,
+            "num" : original_list[item]
+        }
+        dictionary_list.append(info)
+
+    dictionary_list.sort(reverse=True, key=sort_on)    
+    return dictionary_list
+

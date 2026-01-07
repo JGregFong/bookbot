e77cb415a23dcd543a76e82737d0ad7bd9e64d4b
JGregFong <jgregfong@gmail.com>
2026-01-06 17:40:05 -0800

    Updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+import sys
+from stats import get_num_words, get_num_letters, sorted_dictionary
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -6,9 +7,25 @@ def get_book_text(path_to_file):
     
 
 def main():
-    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
+    print("Usage: python3 main.py <path_to_book>")
+    #print(sys.argv)
+ 
+    book_link= sys.argv[1]
+    num_words = get_num_words(get_book_text(book_link))
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at " + book_link + "...")
+    print("----------- Word Count ----------")
     print("Found", num_words, "total words")
+    dictionary = get_num_letters(get_book_text(book_link))
+    updated_dictionary = sorted_dictionary(dictionary)
+    print("--------- Character Count -------")
+    for entry in updated_dictionary:
+        print(entry["char"] + ": " + str(entry["num"]))
     
-    
-    
+    print("============= END ===============")
+
+
+    #sys.exit(1)
+
+
 main()
